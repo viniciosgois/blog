b7e337bdc53a7f6a5b8b2a8c5723a51ad860fb59
vinicios <vinigadd@gmail.com>
2024-01-24 22:22:16 -0300

    feat: add home page text

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <main>Trying to think better on my digital brain.</main>;
+  return (
+    <main className="flex flex-col flex-1">
+      <h2 className="text-2xl font-medium mb-8 tracking-tighter">
+        hey, call me Vini 👋
+      </h2>
+      <p className="mt-4">
+        I'm a software engineer with 4+ years of experience. I currently work as
+        a full-stack developer at Hospital Israelita Albert Einstein, working
+        within the typescript ecosystem.
+      </p>
+    </main>
+  );
 }
