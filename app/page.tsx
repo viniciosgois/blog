5c1f945cdeb4ece55ae5ba5312a79bbe82a3a9f2
vinicios <vinigadd@gmail.com>
2024-01-25 20:49:38 -0300

    fix: unescaped var

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
-    <main className="flex flex-col flex-1">
-      <h2 className="text-2xl font-medium mb-8 tracking-tighter">
+    <main className="flex flex-1 flex-col">
+      <h2 className="mb-8 text-2xl font-medium tracking-tighter">
         hey, call me Vini 👋
       </h2>
       <p className="mt-4">
-        I'm a software engineer with 4+ years of experience. I currently work as
-        a full-stack developer at Hospital Israelita Albert Einstein, working
-        within the typescript ecosystem.
+        I&apos;m a software engineer with 4+ years of experience. I currently
+        work as a full-stack developer at Hospital Israelita Albert Einstein,
+        working within the typescript ecosystem.
       </p>
     </main>
   );
