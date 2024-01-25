01794304978f248c67687ee693d82934b865a5aa
vinicios <vinigadd@gmail.com>
2024-01-24 22:21:39 -0300

    refactor: header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,30 +1,15 @@
 import Link from "next/link";
 
-type Anchor = {
-  name: string;
-  href: string;
-};
+import { navbarAnchors } from "@/app/util/data";
 
 export default function Header() {
-  const anchors: Anchor[] = [
-    {
-      name: "about",
-      href: "/about",
-    },
-  ] as const;
-
   return (
-    <header className="flex justify-between">
-      <Link href="/">
-        <h1 className="font-bold text-2xl">Vinicios Gois</h1>
-      </Link>
-      <div className="flex gap-6">
-        {anchors.map((anchor) => (
-          <Link href={anchor.href}>
-            <span className="capitalize">{anchor.name}</span>
-          </Link>
-        ))}
-      </div>
+    <header className="flex gap-6 justify-center mb-16">
+      {navbarAnchors.map((anchor) => (
+        <Link key={anchor.name} href={anchor.href}>
+          <span className="hover:underline font-medium">{anchor.name}</span>
+        </Link>
+      ))}
     </header>
   );
 }
